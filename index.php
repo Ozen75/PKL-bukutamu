c1084e45d756e39cf3a8c2ee6daf22e0a153ef6e
wow <wowserusekaliwow@gmail.com>
2024-10-14 14:16:38 +0700

    halaman utama (index)

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pro.ASTA</title>
+    <link rel="stylesheet" href="CSS/index.css">
+</head>
+<body>
+    
+    <div class="container">
+        <div class="logo">
+            <img src="images/logo_pro_asta.png" alt="Pro.ASTA Logo">
+        </div>
+        <div class="card">
+            <button class="btn identity">Pilih Identitas</button>
+            <button class="btn masuk" disabled>Masuk</button>
+        </div>
+    </div>
+</body>
+</html>
